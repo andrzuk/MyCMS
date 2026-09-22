1986e2d946d823e246e1e896e3eb8b48377c75ff
Andrzej <andrzuk@wp.pl>
2026-09-22 08:28:13 +0200

    Add IP-based rate limiting for massive attacks.

--- index.php
+++ index.php
@@ -2,11 +2,26 @@
 
 session_start();
 
+error_reporting(E_ALL);
+
 if (isset($_SESSION['last_request_timestamp']))
 {
 	if (microtime(true) - $_SESSION['last_request_timestamp'] < 0.1) exit();
 }
 $_SESSION['last_request_timestamp'] = microtime(true);
+
+$visitor_ip = $_SERVER['REMOTE_ADDR'];
+
+// IP-based rate limiting
+if (!isset($_SESSION['ip_last_request_timestamp'])) 
+{
+	$_SESSION['ip_last_request_timestamp'] = [];
+}
+if (isset($_SESSION['ip_last_request_timestamp'][$visitor_ip])) 
+{
+	if (microtime(true) - $_SESSION['ip_last_request_timestamp'][$visitor_ip] < 0.1) exit();
+}
+$_SESSION['ip_last_request_timestamp'][$visitor_ip] = microtime(true);
 
 include 'config/config.php';
 include LIB_DIR . 'database.php';
@@ -18,15 +33,14 @@ $db = $connection->open();
 include LIB_DIR . 'settings.php';
 include LIB_DIR . 'visitors.php';
 
-$visitor_ip = $_SERVER['REMOTE_ADDR'];
-
 $setting = new Settings($db);
+$visitor = new Visitors($db);
+
 $black_list_visitors = $setting->get_config_key('black_list_visitors');
 $black_list_ip = explode(',', $black_list_visitors);
 foreach ($black_list_ip AS $black_list_item)
 	if ($visitor_ip == trim(str_replace('\'', '', $black_list_item)))
 	{
-		$visitor = new Visitors($db);
 		$visitor->reject();
 		$connection->close($db);
 		http_response_code(401);
@@ -55,7 +69,6 @@ if (!file_exists($route_controller))
 }
 
 include APP_DIR . 'controller/main/status.php';
-		
 include APP_DIR . 'controller/main/acl.php';
 
 if (!file_exists('install/index.php')) 
@@ -70,7 +83,6 @@ else
 
 $_SESSION['last_route'] = isset($_GET['route']) ? $_GET['route'] : NULL;
 
-$visitor = new Visitors($db);
 $visitor->register();
 
 $connection->close($db);
